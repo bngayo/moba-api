2dbca75d1b6c7afdc05a5eb23a08050a4c7b020d
Bramwel <bramwelngayo@gmail.com>
2021-05-19 12:55:38 +0300

    Updated main menu

--- resources/js/Shared/MainMenu.js
+++ resources/js/Shared/MainMenu.js
@@ -5,8 +5,9 @@ export default ({ className }) => {
   return (
     <div className={className}>
       <MainMenuItem text="Dashboard" link="dashboard" icon="dashboard" />
-      <MainMenuItem text="Members" link="organizations" icon="office" />
-      <MainMenuItem text="Payments" link="contacts" icon="users" />
+      <MainMenuItem text="Members" link="users" icon="users" />
+      <MainMenuItem text="Payments" link="contacts" icon="office" />
+      <MainMenuItem text="Users" link="users" icon="users" />
       <MainMenuItem text="Reports" link="reports" icon="printer" />
     </div>
   );
